d9e7376d1079071fff991a8d6f8ad371237ea998
Armando Assembleia <armandoassembleia@Armandos-MacBook-Air.local>
2023-02-06 10:42:46 +0100

    ola

---
+++ file8.py
@@ -0,0 +1,9 @@
+print("Hello")
+
+print("Bye")
+print("Bye2")
+
+print("Hello again")
+print("Hello Hello Hello")
+print("Hello Hello Hello")
+print("adeus")
--- fileseventh.py
+++
@@ -1,7 +0,0 @@
-print("Hello")
-
-print("Bye")
-print("Bye2")
-
-print("Hello again")
-print("Hello Hello Hello")
